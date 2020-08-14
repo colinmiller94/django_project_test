e2653e00274f68542d01b86b67826a8b3cedb4d2
colinmiller94 <40308530+colinmiller94@users.noreply.github.com>
2020-08-14 12:57:06 -0400

    start code

--- rename.py
+++ rename.py
@@ -1,1 +1,57 @@
+import argparse
+import os
+
 DEFAULT_NAME = 'django_project'
+BLACKLIST_DIRS = ['.git', 'migrations', '__pycache__']
+BLACKLIST_FILES = ['.DS_Store', 'db.sqlite3']
+
+THIS_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
+
+
+# helper functions
+def skip(string, skip_list):
+    # truthy if one of BLACKLIST_STRS in the string
+    return sum([s in string for s in skip_list])
+
+
+
+
+
+def rename(directory, old, new):
+
+
+    for root, dirs, files in os.walk(directory, topdown=False):
+        if skip(root, BLACKLIST_DIRS):
+            continue
+        print(root, dirs, files, '\n\n')
+
+        # Alter files first
+        for file in files:
+            if skip(file, BLACKLIST_FILES):
+                continue
+            full_old_path = os.path.join(root, file)
+            full_new_path = os.path.join(root, file.replace(old,new))
+            os.rename(full_old_path,full_new_path)
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Rename the protect. Renames files and replaces references in files',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument('-o', '--old', action='store', type=str, default=DEFAULT_NAME,
+                        help=f'Specify old name with --old=old Default: {DEFAULT_NAME}', dest='old')
+
+    parser.add_argument('-n', '--new', action='store', type=str, default=DEFAULT_NAME,
+                        help=f'Specify new name with --new=new Default: {DEFAULT_NAME}', dest='new')
+
+    parser.add_argument('-d', '--directory', action='store', type=str, default=THIS_DIR,
+                        help=f'Specify directory to alter with --directory=directory Default: {THIS_DIR}',
+                        dest='directory')
+
+
+rename(THIS_DIR, '', '')
